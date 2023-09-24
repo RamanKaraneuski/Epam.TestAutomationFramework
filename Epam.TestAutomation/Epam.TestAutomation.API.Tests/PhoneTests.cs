eb061860bb0ac1667af22dedc139ceebdf8afd8b
RamanKaraneuski <raman.karaneuski@gmail.com>
2023-09-24 13:08:20 +0300

    Last Update Homework Api Testing Two

--- Epam.TestAutomation/Epam.TestAutomation.API.Tests/PhoneTests.cs
+++ Epam.TestAutomation/Epam.TestAutomation.API.Tests/PhoneTests.cs
@@ -14,22 +14,31 @@ namespace Epam.TestAutomation.API.Tests
     [TestFixture]
     public class PhoneTests
     {
+        /// <summary>
+        /// Check that all phones have been received.
+        /// </summary>
         [Test]
         public void VerifyAllPhonesResponse()
         {
             var response = new PhoneController(new CustomRestClient()).GetPhones<List<Phone>>();
-            Assert.That(response.Response.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Error: invalid status code for GET request /objects!");
+            Assert.That(response.Response.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Error: invalid code status for GET request /phones!");
         }
 
+        /// <summary>
+        /// Checks whether information about a specific phone has been received.
+        /// </summary>
         [Test]
         public void VerifySinglePhoneResponse()
         {
             var phone = new PhoneController(new CustomRestClient()).GetPhones<List<Phone>>().AllPhones.First();
 
             var response = new PhoneController(new CustomRestClient()).GetPhone<Phone>(phone.id);
-            Assert.That(response.Response.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Error: invalid status code for GET request /objects/{id}!");
+            Assert.That(response.Response.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Error: invalid code status for GET request /phones/{id}!");
         }
 
+        /// <summary>
+        /// Checking the possibility of adding a phone.
+        /// </summary>
         [Test]
         public void VerifyAbilityToAddPhone()
         {
@@ -49,7 +58,9 @@ namespace Epam.TestAutomation.API.Tests
 
             Assert.That(receivedPhone, Is.Not.Null, $"Error: phone with id {createdPhone.id} was not created!");
         }
-
+        /// <summary>
+        /// Create a phone with a given capacity.
+        /// </summary>
         [Test]
         public void CreatePhoneWithSpecificCapacity()
         {
@@ -64,12 +75,15 @@ namespace Epam.TestAutomation.API.Tests
 
             var createdPhone = new PhoneController(new CustomRestClient()).AddPhone<Phone>(phoneToCreate).Phone;
 
-            Assert.AreEqual(512, createdPhone.data.CapacityGb, "Error: phone capacity is incorrect!");
+            Assert.AreEqual(512, createdPhone.data.CapacityGb, "Error: Invalid phone capacity!");
 
             var response = new PhoneController(new CustomRestClient()).DeletePhone<RestResponse>(createdPhone.id);
-            Assert.AreEqual(HttpStatusCode.OK, response.Response.StatusCode, "Error: incorrect status code when deleting a phone!");
+            Assert.AreEqual(HttpStatusCode.OK, response.Response.StatusCode, "Error: invalid status code when deleting phone!");
         }
 
+        /// <summary>
+        /// Create and delete a random phone.
+        /// </summary>
         [Test]
         public void CreateAndDeleteRandomPhone()
         {
@@ -87,12 +101,15 @@ namespace Epam.TestAutomation.API.Tests
             var createdPhone = new PhoneController(new CustomRestClient()).AddPhone<Phone>(randomPhone).Phone;
 
             var response = new PhoneController(new CustomRestClient()).DeletePhone<RestResponse>(createdPhone.id);
-            Assert.AreEqual(HttpStatusCode.OK, response.Response.StatusCode, "Error: incorrect status code when deleting a random phone!");
+            Assert.AreEqual(HttpStatusCode.OK, response.Response.StatusCode, "Error: invalid status code when deleting a random phone!");
 
             var getResponse = new PhoneController(new CustomRestClient()).GetPhone<Phone>(createdPhone.id);
-            Assert.AreEqual(HttpStatusCode.NotFound, getResponse.Response.StatusCode, "Error: incorrect status code when trying to receive a remote phone!");
+            Assert.AreEqual(HttpStatusCode.NotFound, getResponse.Response.StatusCode, "Error: invalid status code when trying to get a remote phone!");
         }
 
+        /// <summary>
+        /// Update phone properties.
+        /// </summary>
         [Test]
         public void UpdatePhoneProperty()
         {
@@ -122,14 +139,14 @@ namespace Epam.TestAutomation.API.Tests
 
             var updateResponse = new PhoneController(new CustomRestClient()).UpdatePhone<Phone>(createdPhone.id, updatedPhoneModel);
 
-            Assert.AreEqual(HttpStatusCode.OK, updateResponse.Response.StatusCode, "Error: incorrect status code when updating your phone!");
+            Assert.AreEqual(HttpStatusCode.OK, updateResponse.Response.StatusCode, "Error: invalid status code when updating phone!");
 
             var updatedPhone = new PhoneController(new CustomRestClient()).GetPhone<Phone>(createdPhone.id).Phone;
             Assert.AreEqual(1025, updatedPhone.data.CapacityGb, "Error: Incorrect phone capacity after update!");
             Assert.AreEqual(3000, updatedPhone.data.price, "Error: incorrect phone price after update!");
 
             var deleteResponse = new PhoneController(new CustomRestClient()).DeletePhone<RestResponse>(updatedPhone.id);
-            Assert.AreEqual(HttpStatusCode.OK, deleteResponse.Response.StatusCode, "Error: incorrect status code when deleting a phone!");
+            Assert.AreEqual(HttpStatusCode.OK, deleteResponse.Response.StatusCode, "Error: invalid status code when deleting a phone!");
         }
     }
 }
